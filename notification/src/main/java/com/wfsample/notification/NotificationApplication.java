7f0b098c655032508dc134a2470a2ad1aeb60f9d
Hao Song <songhao@vmware.com>
2019-02-08 13:48:01 -0800

    Change to using async calls

--- notification/src/main/java/com/wfsample/notification/NotificationApplication.java
+++ notification/src/main/java/com/wfsample/notification/NotificationApplication.java
@@ -1,11 +1,21 @@
 package com.wfsample.notification;
 
+import com.wavefront.config.WavefrontReportingConfig;
+import com.wavefront.sdk.common.application.ApplicationTags;
+import com.wavefront.sdk.jaxrs.server.WavefrontJaxrsDynamicFeature;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 
 import javax.ws.rs.container.DynamicFeature;
+
+import io.opentracing.Tracer;
+
+import static com.wavefront.config.ReportingUtils.constructApplicationTags;
+import static com.wavefront.config.ReportingUtils.constructWavefrontReportingConfig;
+
 
 /**
  * Main Springboot class for Notification Service.
@@ -20,8 +30,12 @@ public class NotificationApplication {
   }
 
   @Bean
-  public DynamicFeature wavefrontJaxrsFeatureBean(Environment env) {
-    return new WavefrontJaxrsFeature(env);
+  public WavefrontJaxrsDynamicFeature wavefrontJaxrsFeatureBean(Environment env) {
+    ApplicationTags applicationTags =
+        constructApplicationTags(env.getProperty("applicationTagsYamlFile"));
+    WavefrontReportingConfig wfReportingConfig =
+        constructWavefrontReportingConfig(env.getProperty("wfReportingConfigYamlFile"));
+    return new WavefrontJaxrsDynamicFeature(applicationTags, wfReportingConfig);
   }
 
 }
--- notification/src/main/java/com/wfsample/notification/NotificationService.java
+++ notification/src/main/java/com/wfsample/notification/NotificationService.java
@@ -2,34 +2,72 @@ package com.wfsample.notification;
 
 import com.wfsample.service.NotificationApi;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
 
-import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
+
+import io.opentracing.Scope;
+import io.opentracing.Span;
+import io.opentracing.SpanContext;
+import io.opentracing.Tracer;
+import io.opentracing.contrib.concurrent.TracedExecutorService;
+import io.opentracing.propagation.Format;
+import io.opentracing.propagation.TextMap;
+import io.opentracing.propagation.TextMapExtractAdapter;
+import io.opentracing.propagation.TextMapInjectAdapter;
+import io.opentracing.tag.Tags;
+import io.opentracing.util.GlobalTracer;
 
 /**
  * Implementation of Notification Service.
  *
  * @author Hao Song (songhao@vmware.com).
  */
-@Path("/notify")
 @Service
 public class NotificationService implements NotificationApi {
-  AtomicInteger notify = new AtomicInteger(0);
+  private AtomicInteger notify = new AtomicInteger(0);
+
+  private final Tracer tracer;
+
+  @Autowired
+  public NotificationService() {
+    this.tracer = GlobalTracer.get();
+  }
 
   public Response notify(String trackNum) {
-    try {
-      Thread.sleep(100);
-    } catch (InterruptedException e) {
-      e.printStackTrace();
+    Span currentSpan = tracer.scopeManager().active().span();
+    TextMap carrier = new TextMapInjectAdapter(new HashMap<>());
+    tracer.inject(currentSpan.context(), Format.Builtin.HTTP_HEADERS, carrier);
+    ExecutorService executorService = new TracedExecutorService(
+        Executors.newFixedThreadPool(1), tracer);
+    executorService.submit(new InternalNotifyService());
+    return Response.accepted().build();
+  }
+
+  class InternalNotifyService implements Runnable {
+
+    @Override
+    public void run() {
+      TextMap carrier = new TextMapExtractAdapter(new HashMap<>());
+      SpanContext ctx = tracer.extract(Format.Builtin.HTTP_HEADERS, carrier);
+      try (Scope asyncSpan = tracer.buildSpan("asyncNotify").asChildOf(ctx).
+          startActive(true)) {
+        try {
+          Thread.sleep(200);
+          if (notify.incrementAndGet() % 10 == 0) {
+            Tags.ERROR.set(asyncSpan.span(), true);
+          }
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
+      }
     }
-    if (notify.incrementAndGet() % 10 == 0) {
-      return Response.status(Response.Status.SERVICE_UNAVAILABLE).entity("Unable to send " +
-          "notification").build();
-    }
-    return Response.ok("Track Num: " + trackNum + " notified!").build();
   }
 
 }
