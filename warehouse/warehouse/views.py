41aa9bb8402397b171fed4dfedf98e62dd9b128f
Hao Song <songhao@vmware.com>
2019-02-08 12:20:23 -0800

    Remove redundant trace() decorator

--- warehouse/warehouse/views.py
+++ warehouse/warehouse/views.py
@@ -1,17 +1,11 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from django.conf import settings
-import random
-import uuid
 import logging
-
-# tracer = settings.OPENTRACING_TRACING
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-# @tracer.trace()
 @api_view(http_method_names=["GET"])
 def fetch(request, order_num):
     if not order_num:
