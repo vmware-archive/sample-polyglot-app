dfc1fd931d2eae5b3b2cd5a4881b6be925861a16
Vikram Raman <vikram@wavefront.com>
2019-02-09 09:23:21 -0800

    update deps

--- inventory/loadgen.sh
+++ inventory/loadgen.sh
@@ -5,10 +5,10 @@ echo "Send requests every $1 seconds"
 while [ true ]
 do
 	quantity=$((1 + RANDOM % 20))
-	curl_cmd="curl http://localhost:50052/inventory/available/35" 
+	curl_cmd="curl http://localhost:60001/inventory/available/35" 
 	echo $curl_cmd
 	eval $curl_cmd
-	curl_cmd="curl -X POST http://localhost:50050/inventory/checkout/45"
+	curl_cmd="curl -X POST http://localhost:60001/inventory/checkout/45"
 	echo $curl_cmd
 	eval $curl_cmd
 	sleep $1
